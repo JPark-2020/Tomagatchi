e7ad56c1c7fd6c64c8ad4d6c51d3e25c407ea2f8
jonathancode <parkj97@gmail.com>
2021-06-17 18:17:18 -0700

    starting page logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
-// hello 
+// const removeForm = function removeForm(){
+//     $(".name_pet").remove();
+// }
+
+
+const getName = function getName(){
+    document.getElementById('pet_name').innerHTML = document.getElementById("get_name").value;
+}
+
+$("#power_button").click(function(){
+    $(".name_pet").fadeToggle(2000);
+})
+
+$("#name_button").click(function(){
+    getName();
+    $(".name_pet").remove();
+    $("#game_screen").fadeToggle(2000);
+})
+
